b65b1becd80d572372b610628679c35c9b4a0d20
Brett Eiffert <brettceiffert@gmail.com>
2020-05-04 14:50:27 -0500

    refactor

--- start/bokehApp.py
+++ start/bokehApp.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import holoviews.operation.datashader as hd
 import datashader as ds
-from holoviews.operation.datashader import datashade, dynspread
+from holoviews.operation.datashader import datashade, rasterize, dynspread
 from datashader.colors import Sets1to3
 import pandas as pd
 
@@ -13,8 +13,6 @@ from bokeh.io import show
 from bokeh.layouts import layout, gridplot
 from bokeh.models import Slider, Button, AutocompleteInput, Label, Select
 from bokeh.plotting import curdoc
-
-import math
 
 import colorcet as cc
 
@@ -34,10 +32,11 @@ db=client.admin
 hv.extension('bokeh', 'matplotlib')
 renderer = hv.renderer('bokeh').instance(mode='server')
 
-
+#get list of named genes - all - full view
 def getGenes():
     return db.genesfullv2.distinct('name')
 
+#get expression values for each cell given the gene - full view
 def getVals(gene):
     query = db.genesfullv2.find({'name' : gene})
     
@@ -47,14 +46,15 @@ def getVals(gene):
 
     arr = np.zeros(346264, dtype=int)
     for key in temp:
-
         arr[int(key)] = 1
 
     return arr
 
+#get list of named genes
 def getGenes_microglia():
     return db.genesfullmicroglia.distinct('name')
 
+#get expression values for each cell given the gene - microglia specific full view
 def getVals_microglia(gene):
     query = db.genesfullmicroglia.find({'name' : gene})
     
@@ -66,82 +66,72 @@ def getVals_microglia(gene):
     for key in temp:
         arr[int(key)] = 1
 
-
     return arr
 
-def basegraph(gene, set):
-	#print(gene)
-	if (set == "all"):
-		return hv.Points((df["UMAP_1"], df["UMAP_2"], getVals(gene)), ['UMAP_1', 'UMAP_2'], vdims='Category')
-	elif (set == "microglia"):
-		return hv.Points((df3["UMAP_1"], df3["UMAP_2"], getVals_microglia(gene)), ['UMAP_1', 'UMAP_2'], vdims='Category')
+#queried graph view based on catergory of brain cell
+def querygraph(gene, set):
+	if (set == "microglia"):
+		categorical_points = hv.Points((df3["UMAP_1"], df3["UMAP_2"], getVals_microglia(gene)), ['UMAP_1', 'UMAP_2'], vdims='Category')
+	else:
+		categorical_points = hv.Points((df["UMAP_1"], df["UMAP_2"], getVals(gene)), ['UMAP_1', 'UMAP_2'], vdims='Category')
 
-def fullgraph(set):
-	if (set == "all"):
-		return hv.Points((df["UMAP_1"], df["UMAP_2"], df2["SCT_snn_res.0.2"]), ['UMAP_1', 'UMAP_2'], vdims='Category')
-	elif (set == "microglia"):
-		return hv.Points((df3["UMAP_1"], df3["UMAP_2"], df4["SCT_snn_res.0.2"]), ['UMAP_1', 'UMAP_2'], vdims='Category')
+	return dynspread(datashade(categorical_points, aggregator=ds.count_cat('Category'), color_key=['#c9c9c9', '#00008a'], dynamic=False).opts(plot=dict(width=600, height=450)))
+
+#clustered view based on category of brain cell
+def clustersgraph(set):
+	if (set == "microglia"):
+		categorical_points = hv.Points((df3["UMAP_1"], df3["UMAP_2"], df4["SCT_snn_res.0.2"]), ['UMAP_1', 'UMAP_2'], vdims='Category')
+	else:
+		categorical_points = hv.Points((df["UMAP_1"], df["UMAP_2"], df2["SCT_snn_res.0.2"]), ['UMAP_1', 'UMAP_2'], vdims='Category')
+
+	return dynspread(datashade(categorical_points, aggregator=ds.count_cat('Category'), color_key=Sets1to3, dynamic=False).opts(plot=dict(width=600, height=450)))
 
 
+#streams - used for "live updates"
 stream = hv.streams.Stream.define('Gene', gene="", set="all")()
 streamSet = hv.streams.Stream.define('Set', set="all")()
 
 
-dmap = dynspread(datashade(hv.DynamicMap(basegraph, streams=[stream]), aggregator=ds.count_cat('Category'), color_key=['#c9c9c9', '#00008a']).opts(plot=dict(width=600, height=450)))
+# graph variables - calls function to get graph objects
+dmap_query = hv.DynamicMap(querygraph, streams=[stream])
+dmap_cluster = hv.DynamicMap(clustersgraph, streams=[streamSet])
 
 
-dmap_full = dynspread(datashade(hv.DynamicMap(fullgraph, streams=[streamSet]), aggregator=ds.count_cat('Category'), color_key=Sets1to3).opts(plot=dict(width=600, height=450)))
 
+#callbacks intitialized and called here
 def modify_doc(doc):
 
-	def ac_update(attrname, old, new):
+	#callback function
+	def autocomplete_update(attrname, old, new):
 		stream.event(gene=new)
-		print(ac.value)
+		print(autocomplete.value)
 
-	def dd_update(attrname, old, new):
+	#callback function
+	def dropdown_update(attrname, old, new):
 		streamSet.event(set=new)
 		stream.event(set=new)
-		print(dd.value)
+		print(dropdown.value)
 
-	dd = Select(title="Cell Set", value="all",
+	#initialize dropdown to select cell type
+	dropdown = Select(title="Cell Set", value="all",
                    options=['all', 'microglia'])
-	ac = AutocompleteInput(title="Type in a gene and select from the dropdown", value="", completions=getGenes())
 
-	dd.on_change('value', dd_update)
-	ac.on_change('value', ac_update)
+	#initialize autocomplete field for querying genes
+	autocomplete = AutocompleteInput(title="Type in a gene and select from the dropdown", value="", completions=getGenes())
 
-	hvplot = renderer.get_plot(dmap, doc)
-	hvplot_full = renderer.get_plot(dmap_full, doc)
+	#callbacks
+	dropdown.on_change('value', dropdown_update)
+	autocomplete.on_change('value', autocomplete_update)
 
-	plot = gridplot([[ac, dd], [hvplot.state, hvplot_full.state]])
+	#get graphs and store in variables
+	hvplot = renderer.get_plot(dmap_query, doc)
+	hvplot_full = renderer.get_plot(dmap_cluster, doc)
 
+	#create view for HTML
+	plot = gridplot([[autocomplete, dropdown], [hvplot.state, hvplot_full.state]])
+
+	#add view to HTML doc
 	doc.add_root(plot)
-# To display in a script
+
+#call function modify doc to display
 doc = modify_doc(curdoc())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #categorical_points = hv.Points((df["UMAP_1"], df["UMAP_2"], df2["SCT_snn_res.0.2"]), ['UMAP_1', 'UMAP_2'], vdims='Category')
-
-    #dmap = dynspread(datashade(categorical_points, aggregator=ds.count_cat('Category'), color_key=Sets1to3).opts(plot=dict(width=600, height=450)))
-
-    #app = renderer.app(dmap)
-    #server = Server({'/': app}, port=8050, allow_websocket_origin=["fenix.psych.wucon.wustl.edu:5001"]) #####################
-    #server.start()
-    #loop = IOLoop.current()
-    #loop.start()
